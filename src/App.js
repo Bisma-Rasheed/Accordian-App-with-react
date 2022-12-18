ca808fcb6e6c38a2ebcad9465d9a2ea9c36a20e4
Bisma-Rasheed <bismarasheed95@gmail.com>
2022-12-18 13:19:35 +0500

    version 1.0.0

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+    return (
+        <>
+        <h1>hello world</h1>
+        <button className="btn btn-success">click me</button>
+        </>
+    )
+}
+
+export default App;
